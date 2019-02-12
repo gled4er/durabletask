0e49ee66e81111a867a0007fbc78a7d5167910ad
Kanio Dimitrov <kadimitr@microsoft.com>
2019-02-11 16:24:46 -0800

    Fixing project strucutre.

---
+++ Test/DurableTask.Redis.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DurableTask.Redis.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
